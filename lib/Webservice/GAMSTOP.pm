18a8e40cfe35ee14b9b0b51c352a566ea2a82e3f
Raunak Kathuria <raunakkathuria@gmail.com>
2018-02-23 07:50:48 +0000

    initial structure

---
+++ lib/Webservice/GAMSTOP.pm
@@ -0,0 +1,47 @@
+package Webservice::GAMSTOP;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Webservice::GAMSTOP - Module abstract
+
+=head1 SYNOPSIS
+
+    use Webservice::GAMSTOP;
+    my $instance = Webservice::GAMSTOP->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+=head1 AUTHOR
+
+binary.com <cpan@binary.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2018 by binary.com.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
